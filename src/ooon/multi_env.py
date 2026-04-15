4d4cf2b44ca5ab533e470d20a3368202ef7c125b
zhaihao <zhaihao@ooon.me>
2026-04-15 15:53:37 +0800

    env settings

--- src/ooon/multi_env.py
+++ src/ooon/multi_env.py
@@ -1,11 +1,19 @@
 import os
 
 from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 def init_env():
-    env = os.getenv("env", "dev")
+    env = os.getenv("APP_ENV", "dev")
     env_file = f".env.{env}"
     if os.path.exists(env_file):
         print(f"loading {env} config")
-    load_dotenv(env_file)
+        load_dotenv(env_file)
     load_dotenv()
+
+class EnvSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env", f".env.{os.getenv('APP_ENV', 'dev')}"),
+        extra='ignore' # 忽略多余的环境变量
+    )
